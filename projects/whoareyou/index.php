f9d6a6284e4b3aa975dfa7cb3692af23714ff6e6
BadPingHere <62813557+BadPingHere@users.noreply.github.com>
2022-12-02 17:28:46 -0500

    add new projects

--- projects/whoareyou/index.php
+++ projects/whoareyou/index.php
@@ -4,6 +4,13 @@ $time = date("Y-m-d h:i:s");
 $headers = array_change_key_case(getallheaders(), CASE_LOWER); 
 $response_code = http_response_code()
 ?>
+<?php
+
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
